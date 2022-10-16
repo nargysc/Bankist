e2ba23b68999b3ccda72a6215f10a7ef0381364b
Nargys <32126532+nargysc@users.noreply.github.com>
2022-10-16 15:01:03 +0300

    Added Cookie section

--- script.js
+++ script.js
@@ -38,7 +38,7 @@ console.log(document.documentElement);
 console.log(document.head);
 console.log(document.body);
 
-document.querySelector('.header');
+const header = document.querySelector('.header');
 const allSections = document.querySelectorAll('.section');
 console.log(allSections);
 
@@ -46,3 +46,20 @@ console.log(allSections);
 document.getElementById('section--1');
 const allButtons = document.getElementsByTagName('button');
 console.log(allButtons);
+
+console.log(document.getElementsByClassName('btn'));
+
+//creating and inserting eleements
+
+/*Inserting HTML element*/
+
+const message = document.createElement('div');
+message.classList.add('cookie-message');
+
+message.innerHTML = 'We use cookies for imroved functionality and analytics. <button class="btn btn--close-cookie">Got it</button>';
+
+//header.prepend(message);
+header.prepend(message);
+//header.append(message);
+
+header.append(message.cloneNode(true));
