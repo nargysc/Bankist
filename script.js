949e9a8bbaff3aae28988a6a9ecda368387036e4
Nargys <32126532+nargysc@users.noreply.github.com>
2022-10-16 16:24:39 +0300

    Addede attributes and classes to JS

--- script.js
+++ script.js
@@ -89,3 +89,35 @@ console.log(getComputedStyle(message).height);
 message.style.height = Number.parseFloat(getComputedStyle(message).height,10) + 30 + 'px';
 
 document.documentElement.style.setProperty('--color-primary', 'orangered');
+
+/*Logo content*/
+//Attributes 
+
+const logo = document.querySelector('.nav__logo');
+console.log(logo.alt);
+console.log(logo.className);
+
+logo.alt = 'Beautiful minimalist logo';
+
+//non-standart 
+console.log(logo.designer);
+console.log(logo.getAttribute('designer'));
+logo.setAttribute('company', 'Bankist');
+
+console.log(logo.src);
+console.log(logo.getAttribute('src'));
+
+
+const link = document.querySelector('.nav__link--btn');
+console.log(link.href);
+console.log(link.getAttribute('href'));
+
+console.log(logo.dataset.versionNumber);
+
+//Classes 
+logo.classList.add('c', 'j');
+logo.classList.remove('c', 'j');
+logo.classList.toggle('c');
+logo.classList.contains('c');
+
+logo.className = 'nargys';
